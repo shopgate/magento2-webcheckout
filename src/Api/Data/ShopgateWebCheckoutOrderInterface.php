0ce5553ecc455e9150a654996ac6710e9933907f
André Kraus <andre.kraus@apite.io>
2024-07-15 13:52:43 +0200

    SG-244: added webcheckout order table

---
+++ src/Api/Data/ShopgateWebCheckoutOrderInterface.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Shopgate\WebCheckout\Api\Data;
+interface ShopgateWebCheckoutOrderInterface
+{
+    public final const ENTITY_ID = 'entity_id';
+    public final const ORDER_ID = 'order_id';
+    public final const USER_AGENT = 'user_agent';
+    public final const CREATED_AT = 'created_at';
+    public final const UPDATED_AT = 'updated_at';
+    
+    /**
+     * Get entity id
+     *
+     * @return int
+     */
+    public function getEntityId(): int;
+
+    /**
+     * @return int
+     */
+    public function getOrderId(): int;
+
+    /**
+     * @param int $orderId
+     *
+     * @return void
+     */
+    public function setOrderId(int $orderId): void;
+
+    /**
+     * @return string
+     */
+    public function getUserAgent(): string;
+
+    /**
+     * @param string $userAgent
+     *
+     * @return void
+     */
+    public function setUserAgent(string $userAgent): void;
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt(): string;
+
+    public function getUpdatedAt(): string;
+}
---
+++ src/Model/ResourceModel/ShopgateWebCheckoutOrder.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Shopgate\WebCheckout\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Shopgate\WebCheckout\Api\Data\ShopgateWebCheckoutOrderInterface;
+
+class ShopgateWebCheckoutOrder extends AbstractDb
+{
+    public final const TABLE_NAME = 'shopgate_webcheckout_order';
+
+    protected function _construct()
+    {
+        $this->_init(self::TABLE_NAME, ShopgateWebCheckoutOrderInterface::ENTITY_ID);
+    }
+}
